845976c7960ad128ba6089e1349b1d60d16f2fef
Steve Johnson <snomofomo@gmail.com>
2017-04-07 17:11:29 -0500

    fix okcn

--- get_okcn.php
+++ get_okcn.php
@@ -1,5 +1,5 @@
 <?php
-$query = 'INSERT INTO `current` VALUES';
+
 $time_start = microtime(true);
 
 require 'creds.php';
@@ -23,6 +23,7 @@ $okcnPairs = array('btc','ltc');
 $usdurl = 'https://www.okcoin.com/api/v1/ticker.do?symbol=';
 $cnyurl = 'https://www.okcoin.cn/api/v1/ticker.do?symbol=';
 foreach ($okcnPairs as $key => $coin) {
+  $query = 'INSERT INTO `current` VALUES';
   $tmpusd = getData($usdurl.$coin.'_usd');
   $okcnD1 = $tmpusd['ticker'];
   $query .= '(null,"okcn","'.$coin.'_usd'.'",'.$okcnD1['last'].','.$okcnD1['vol'].',"'.timeStamp().':00"),';
