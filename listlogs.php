ea63a0dad170eea0e1285f30a286b642c2a62501
Steve Johnson <snomofomo@gmail.com>
2017-04-10 14:54:01 -0500

    add links and formatting to listlogs.php - fix typos

--- listlogs.php
+++ listlogs.php
@@ -4,14 +4,16 @@ $dir = '/home/snomofomo/public_html/indxlogs/';
 if(is_dir($dir)){
   if($dh = opendir($dir)){
     while(($file = readdir($dh)) !== false){
-      $tnsp = explode(".",$file);
-      $nsp = explode("_",$tnsp);
-      if($nsp[0] == '00'){
-        $fname = $nsp[1].' : '.$nsp[3].' '.$tsp[4];
-      }else{
-        $fname = $nsp[0].' : '.$nsp[2].' '.$tsp[3];
+      if($file != '.' && $file != '..'){
+        $tnsp = explode(".",$file);
+        $nsp = explode("_",$tnsp);
+        if($nsp[0] == '00'){
+          $fname = $nsp[1].' : '.$nsp[3].' '.$tsp[4];
+        }else{
+          $fname = $nsp[0].' : '.$nsp[2].' '.$tsp[3];
+        }
+        echo '<a href="'.$file.'" target="_blank">'.$fname.'</a><br>';
       }
-      echo '<a href="'$file.'" target="_blank">'.$fname.'</a><br>';
     }
     closedir($dh);
   }
