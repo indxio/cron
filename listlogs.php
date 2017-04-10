a60755bc802c7eef89bcd948a17179a417248057
Steve Johnson <snomofomo@gmail.com>
2017-04-10 14:50:39 -0500

    add links and formatting to listlogs.php - fix

--- listlogs.php
+++ listlogs.php
@@ -4,13 +4,12 @@ $dir = '/home/snomofomo/public_html/indxlogs/';
 if(is_dir($dir)){
   if($dh = opendir($dir)){
     while(($file = readdir($dh)) !== false){
-      $nsp = explode("_",$file);
+      $tnsp = explode(".",$file);
+      $nsp = explode("_",$tnsp);
       if($nsp[0] == '00'){
-        $tsp = explode('.',$nsp[4]);
-        $fname = $nsp[1].' : '.$nsp[3].' '.$tsp[0];
+        $fname = $nsp[1].' : '.$nsp[3].' '.$tsp[4];
       }else{
-        $tsp = explode('.',$nsp[3]);
-        $fname = $nsp[0].' : '.$nsp[2].' '.$tsp[0];
+        $fname = $nsp[0].' : '.$nsp[2].' '.$tsp[3];
       }
       echo '<a href="'$file.'" target="_blank">'.$fname.'</a><br>';
     }
